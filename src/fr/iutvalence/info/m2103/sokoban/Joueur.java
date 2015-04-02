1fc2d73ec63367905241794e90a1e99e01c73821
bertholet <mathie.bertholet@iut-valence.fr>
2015-04-02 09:50:39 +0200

    Ajout de constructeur et d'une méthode:
    - ajout d'un attribut position
    - ajout constructeur avec paramètre
    - ajout de la méthode deplacer

--- src/fr/iutvalence/info/m2103/sokoban/Joueur.java
+++ src/fr/iutvalence/info/m2103/sokoban/Joueur.java
@@ -2,4 +2,28 @@ package fr.iutvalence.info.m2103.sokoban;
 
 public class Joueur {
 
+	/**
+	 * Position du joueur
+	 */
+	private Position position;
+	
+	
+	/**
+	 * Construit un joueur a une position donnée
+	 * @param pos La position de départ
+	 */
+	public Joueur(Position pos){
+		this.position = pos;
+	}
+	
+	/**
+	 * Deplace le joueur 
+	 * @param deplacement Le déplacement relatif au joueur
+	 * @param niveau Le niveau où le joueur se déplace
+	 */
+	public void deplacer(Position deplacement, Niveau niveau){
+		this.position = new Position(this.position.getPosX() + deplacement.getPosX(), this.position.getPosY() + deplacement.getPosY());
+		// TODO Verifier les règles de déplacements (sortie de terrain, collision)
+	}
+	
 }
