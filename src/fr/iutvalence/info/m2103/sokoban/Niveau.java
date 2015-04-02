873711eae9a57eed954ad82d53dc4e00d849a308
bertholet <mathie.bertholet@iut-valence.fr>
2015-04-02 09:16:04 +0200

    Ajout de deux constructeurs:
    - constructeur sans paramètres: crée niveau par défaut
    - constructeur avec paramètre: crée niveau donné

--- src/fr/iutvalence/info/m2103/sokoban/Niveau.java
+++ src/fr/iutvalence/info/m2103/sokoban/Niveau.java
@@ -1,5 +1,30 @@
 package fr.iutvalence.info.m2103.sokoban;
 
-public class Terrain {
+public class Niveau {
+	
+	/**
+	 * Numéro du niveau par défaut
+	 */
+	public static final int NIVEAU_PAR_DEFAUT = 1;
+	
+	/**
+	 * Numéro du niveau
+	 */
+	private int numeroNiveau;
+	
+	/**
+	 * Construit le niveau par défaut
+	 */
+	public Niveau(){
+		this.numeroNiveau = Niveau.NIVEAU_PAR_DEFAUT;
+	}
+	
+	/**
+	 * Construit un niveau
+	 * @param numeroNiveau le numéro de niveau a créer
+	 */
+	public Niveau(int numeroNiveau){
+		this.numeroNiveau = numeroNiveau;
+	}
 	
 }
