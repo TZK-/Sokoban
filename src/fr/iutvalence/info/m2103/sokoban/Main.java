d2d08bbdbc55e5b1595a22f66b6c19db079adffe
bertholet <mathie.bertholet@iut-valence.fr>
2015-04-02 08:38:51 +0200

    Creation de la classe PartieSokoban
    - gere une partie de sokoban

---
+++ src/fr/iutvalence/info/m2103/sokoban/Main.java
@@ -0,0 +1,11 @@
+package fr.iutvalence.info.m2103.sokoban;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new PartieSokoban().jouer();
+
+	}
+
+}
---
+++ src/fr/iutvalence/info/m2103/sokoban/PartieSokoban.java
@@ -0,0 +1,9 @@
+package fr.iutvalence.info.m2103.sokoban;
+
+public class PartieSokoban {
+
+	public void jouer() {
+		
+	}
+
+}
