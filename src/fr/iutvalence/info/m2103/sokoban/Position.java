d0046cdcf303d6abaf2d06b10e8429d7571d861a
bertholet <mathie.bertholet@iut-valence.fr>
2015-04-02 09:22:58 +0200

    Ajout de la documentation

--- src/fr/iutvalence/info/m2103/sokoban/Position.java
+++ src/fr/iutvalence/info/m2103/sokoban/Position.java
@@ -12,21 +12,37 @@ public class Position {
 	 */
 	private final int posY;
 	
-	
+	/**
+	 * Construit une position par defaut: (0, 0)
+	 */
 	public Position(){
 		this.posX = 0;
 		this.posY = 0;
 	}
 	
+	/**
+	 * Construit une position
+	 * @param posX La position en abscisse
+	 * @param posY La position en ordonnée
+	 */
 	public Position(int posX, int posY){
 		this.posX = posX;
 		this.posY = posY;
 	}
 
+	/**
+	 * Retourne la valeur de l'abscisse
+	 * @return La valeur de l'abscisse
+	 */
 	public int getPosX() {
 		return posX;
 	}
 
+	
+	/**
+	 * Retourne la valeur de l'ordonnée
+	 * @return La valeur de l'ordonnée
+	 */
 	public int getPosY() {
 		return posY;
 	}
