2817b1fad3760379023d4c9912d1936e8e25e7c5
TZK <thib-17@orange.fr>
2015-04-15 19:06:25 +0200

    Adding condition to isValidPosition method      - if the pos equals to null, return false

--- src/fr/iutvalence/info/m2103/sokoban/Level.java
+++ src/fr/iutvalence/info/m2103/sokoban/Level.java
@@ -6,30 +6,30 @@ package fr.iutvalence.info.m2103.sokoban;
  *
  */
 public class Level {
-	
+
 	/**
 	 * Default level number
 	 */
 	public static final int DEFAULT_MAP_SIZE = 5;
-	
+
 	/**
 	 * Array of map elements
 	 * Represents the map of the level
 	 */
 	private MapElement[][] map;
-	
+
 	/**
 	 * Level number
 	 */
 	private int levelNumber;
-	
+
 	/**
 	 * Creates a new level
 	 */
 	public Level(){
 		this.levelNumber = 1;
 		this.map = new MapElement[DEFAULT_MAP_SIZE][DEFAULT_MAP_SIZE];
-		
+
 		for (int line = 0; line < DEFAULT_MAP_SIZE; line++) {
 			for (int column = 0; column < DEFAULT_MAP_SIZE; column++) {
 				if(line == 0 || line == 4)
@@ -44,7 +44,7 @@ public class Level {
 		this.placeElement(new Position(1, 2), MapElement.BOX);
 		this.placeElement(new Position(1, 3), MapElement.TARGET);
 	}
-	
+
 	/**
 	 * Places a specified element at a given position
 	 * @param pos The position
@@ -78,11 +78,12 @@ public class Level {
 	 */
 	private boolean isValidPosition(Position pos){
 		if((pos.getPosX() < 0 || pos.getPosX() > this.map.length)
-			|| (pos.getPosY() < 0 || pos.getPosY() > this.map.length))
+				|| (pos.getPosY() < 0 || pos.getPosY() > this.map.length)
+				|| pos == null)
 			return false;
 		return true;
 	}
-	
+
 	/**
 	 * Returns the starting position where the player will begin
 	 * @return The starting position
@@ -91,8 +92,8 @@ public class Level {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
+
+
 	/**
 	 * Return an ASCII representation of the map
 	 */
@@ -107,5 +108,5 @@ public class Level {
 		}
 		return str;
 	}
-	
+
 }
