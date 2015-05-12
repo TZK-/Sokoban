429e9638e747e0d42b4ec553b7b7e311fd339587
granadat <granadat@PC-B101-03.iut-valence.fr>
2015-05-12 15:15:18 +0200

    Adding deltaX and deltaY + getDeltaPosition method

--- src/fr/iutvalence/info/m2103/sokoban/Direction.java
+++ src/fr/iutvalence/info/m2103/sokoban/Direction.java
@@ -8,21 +8,64 @@ public enum Direction {
 	/**
 	 * The up direction
 	 */
-	UP,
+	UP(-1,0),
 	
 	/**
 	 * The down direction
 	 */
-	DOWN,
+	DOWN(1,0),
 	
 	/**
 	 * The left direction
 	 */
-	LEFT,
+	LEFT(0,-1),
 	
 	/**
 	 * The right direction
 	 */
-	RIGHT;
+	RIGHT(0,1);
+
+	/**
+	 * The delta on the X-Axis
+	 */
+	private final int deltaX;
 	
+	/**
+	 * The delta on the Y-Axis
+	 */
+	private final int deltaY;
+
+	/**
+	 * Creates a new Direction
+	 * @param deltaX the delta X
+	 * @param deltaY the delta Y
+	 */
+	private Direction(int deltaX, int deltaY){
+		this.deltaX = deltaX;
+		this.deltaY = deltaY;
+	}
+
+	/**
+	 * Gets the delta X
+	 * @return the delta X
+	 */
+	public int getDeltaX() {
+		return this.deltaX;
+	}
+
+	/**
+	 * Gets the delta Y
+	 * @return the delta Y
+	 */
+	public int getDeltaY() {
+		return this.deltaY;
+	}
+	
+	/**
+	 * Gets the position formed by delta X and delta Y
+	 * @return the position formed by delta X and delta Y
+	 */
+	public Position getDeltaPosition(){
+		return new Position(this.deltaX, this.deltaY);
+	}
 }
