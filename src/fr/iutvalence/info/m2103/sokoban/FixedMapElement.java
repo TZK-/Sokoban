985feefa748138bac2d9595cf458167ad8aece04
TZK <thib-17@orange.fr>
2015-05-17 16:55:45 +0200

    Adding fromString method. It returns the Enum field which match with the string in parameters.

--- src/fr/iutvalence/info/m2103/sokoban/FixedMapElement.java
+++ src/fr/iutvalence/info/m2103/sokoban/FixedMapElement.java
@@ -32,13 +32,28 @@ public enum FixedMapElement {
 	 * Creates a new Element with a string representation
 	 * @param asciiString The string representation
 	 */
-	FixedMapElement(String asciiString){
+	private FixedMapElement(String asciiString){
 		this.asciiString = asciiString;
 	}
 
 	@Override
 	public String toString(){
 		return this.asciiString;
+	}
+
+	/**
+	 * Returns the Enum field which match with the string in parameters
+	 * @param str The string of the enum+
+	 * @return The Enum field which match with the string in parameters
+	 * @throws IllegalArgumentException Raised if the <tt>str</tt> parameter does not match
+	 *           with an element of the Enumeration.
+	 */
+	public static FixedMapElement fromString(String str) throws IllegalArgumentException{
+		for (FixedMapElement element : FixedMapElement.values()) {
+			if(element.toString().equals(str))
+				return element;
+		}
+		throw new IllegalArgumentException("The " + str + " value does not match with an element of the map");
 	}
 	
 }
