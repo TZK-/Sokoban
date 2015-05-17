b248e1a11a1fdec26bde8e1cec6b57155ab460a5
TZK <thib-17@orange.fr>
2015-05-17 19:24:22 +0200

    Adding PlayerNotPlacedException. Raised if the player has not benn placed on the map.

---
+++ src/fr/iutvalence/info/m2103/exceptions/PlayerNotPlacedException.java
@@ -0,0 +1,13 @@
+package fr.iutvalence.info.m2103.exceptions;
+
+/**
+ * Exception raised when the player is not placed on the map.
+ */
+public class PlayerNotPlacedException extends Exception {
+
+	/**
+	 * Serial UID
+	 */
+	private static final long serialVersionUID = 2483024577938666368L;
+
+}
